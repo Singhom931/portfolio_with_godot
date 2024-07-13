8254018cff87c3d47adb6fee4a18788ce33543eb
Singhom931 <singhom931@gmail.com>
2024-07-14 00:55:48 +0530

    update

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1720062301|6301045070";
+const CACHE_VERSION = "1720898569|10876200804";
 const CACHE_PREFIX = "Portfolio-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
